00b44b1c81eb761d09abfeb8f4d406d06dcea1e7
mberry87 <berry311287@gmail.com>
2023-12-31 15:25:42 +0700

    update script bot.py

--- bot.py
+++ bot.py
@@ -32,141 +32,6 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-# ========= Funsi CRUD ========= #
-
-# Fungsi decorator untuk mengatur waktu logout jika tidak ada aktivitas
-def auto_logout(timeout):
-    def decorator(func):
-        @wraps(func)
-        async def wrapper(update, context):
-            chat_id = update.effective_chat.id
-            user_data = context.user_data.setdefault(chat_id, {})
-            last_activity = user_data.get('last_activity', time.time())
-
-            # Menghitung selisih waktu sejak aktivitas terakhir
-            elapsed_time = time.time() - last_activity
-
-            # Mengatur waktu aktivitas terakhir
-            user_data['last_activity'] = time.time()
-
-            result = await func(update, context)
-
-            # Logout jika tidak ada aktivitas dalam waktu yang ditentukan
-            if elapsed_time > timeout:
-                del context.user_data[chat_id]  # Hapus data pengguna dari user_data
-
-            return result
-        return wrapper
-    return decorator
-
-@auto_logout(timeout=60)
-async def login_data(update: Update, context: CallbackContext) -> None:
-    # Cek apakah format perintah /login valid
-    try:
-        username, password = update.message.text.split()[1:]
-    except ValueError:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Format perintah salah. Contoh: /login username password")
-        return
-
-    # Verifikasi apakah username dan password valid
-    is_admin = False
-    sql = "SELECT * FROM admin WHERE username = %s AND password = %s"
-    val = (username, password)
-    mycursor.execute(sql, val)
-    result = mycursor.fetchone()
-    if result:
-        is_admin = True
-
-    # Berikan tindakan sesuai dengan status admin
-    if is_admin:
-        context.user_data[update.effective_chat.id] = {'admin': True}
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Anda telah berhasil login sebagai admin.")
-    else:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Username atau password salah.")
-
-@auto_logout(timeout=60)
-async def read_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Periksa apakah pengguna memiliki akses admin
-    if not context.user_data.get(update.effective_chat.id, {}).get('admin', False):
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Maaf, hanya admin yang dapat mengakses fungsi ini.")
-        return
-
-    # Jika pengguna adalah admin, jalankan kode normal untuk membaca data
-    sql = "SELECT * FROM pengguna"
-    mycursor.execute(sql)
-    result = mycursor.fetchall()
-    message = "Data yang tersimpan di database:\n\n"
-    for row in result:
-        message += f"ID: {row[0]},\nNama: {row[1]},\nWaktu Daftar: {row[2]},\nLatitude: {row[3]},\nLongitude: {row[4]},\nChat ID: {row[5]}\n\n"
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-@auto_logout(timeout=60)
-async def create_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Periksa apakah pengguna memiliki akses admin
-    if not context.user_data.get(update.effective_chat.id, {}).get('admin', False):
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Maaf, hanya admin yang dapat mengakses fungsi ini.")
-        return
-
-    # Jika pengguna adalah admin, jalankan kode normal untuk membuat data
-    data = update.message.text.split()[1:]
-    sql = "INSERT INTO pengguna (nama, waktu_daftar, latitude, longitude, chat_id) VALUES (%s, %s, %s, %s, %s)"
-    val = tuple(data)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Data berhasil ditambahkan!")
-
-@auto_logout(timeout=60)
-async def edit_data(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    # Periksa apakah pengguna memiliki hak akses
-    if not context.user_data.get(chat_id, {}).get('admin', False):
-        await context.bot.send_message(chat_id=chat_id, text="Anda tidak memiliki hak akses.")
-        return
-
-    # Ambil data yang ingin diubah dari pesan pengguna
-    try:
-        id, kolom, nilai_baru = update.message.text.split()[1:]
-    except ValueError:
-        await context.bot.send_message(chat_id=chat_id, text="Format pesan tidak valid.")
-        return
-
-    # Update data sesuai dengan id, kolom, dan nilai baru yang diberikan
-    try:
-        sql = f"UPDATE pengguna SET {kolom} = %s WHERE id = %s"
-        val = (nilai_baru, id)
-        mycursor.execute(sql, val)
-        mydb.commit()
-        await context.bot.send_message(chat_id=chat_id, text="Data berhasil diperbarui!")
-    except Exception as e:
-        await context.bot.send_message(chat_id=chat_id, text=f"Terjadi kesalahan: {e}")
-
-@auto_logout(timeout=60)
-async def delete_data(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    # Periksa apakah pengguna memiliki hak akses
-    if not context.user_data.get(chat_id, {}).get('admin', False):
-        await context.bot.send_message(chat_id=chat_id, text="Anda tidak memiliki hak akses.")
-        return
-    
-    # Ambil id data yang ingin dihapus dari pesan pengguna
-    try:
-        id = update.message.text.split()[1]
-    except IndexError:
-        await context.bot.send_message(chat_id=chat_id, text="Format pesan tidak valid.")
-        return
-
-    # Hapus data dari database sesuai dengan id yang diberikan
-    try:
-        sql = "DELETE FROM pengguna WHERE id = %s"
-        val = (id,)
-        mycursor.execute(sql, val)
-        mydb.commit()
-        await context.bot.send_message(chat_id=chat_id, text="Data berhasil dihapus!")
-    except Exception as e:
-            await context.bot.send_message(chat_id=chat_id, text=f"Terjadi kesalahan: {e}")
-            
-# =============================== #
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Selamat Datang di Apliaski SiabsenQU")
 
@@ -399,6 +264,7 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token(
         '6160156503:AAF3D2SxB31ibEKsp67dIJZjcfp118qXHxo').build()
 
+    # command bot telegram
     start_handler = CommandHandler('start', start)
     absen_handler = CommandHandler('absen', absen)
     daftar_handler = CommandHandler('daftar', daftar)
@@ -407,6 +273,7 @@ if __name__ == '__main__':
     izin_handler = CommandHandler('izin', izin)
     alasan_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), alasan)
 
+    # handler bot telegram
     application.add_handler(start_handler)
     application.add_handler(absen_handler)
     application.add_handler(daftar_handler)
@@ -414,20 +281,5 @@ if __name__ == '__main__':
     application.add_handler(sakit_handler)
     application.add_handler(izin_handler)
     application.add_handler(alasan_handler)
-
-
-    # Command CRUD
-    login_handler = CommandHandler('login',login_data)
-    read_handler = CommandHandler('read', read_data)
-    create_handler = CommandHandler('create', create_data)
-    edit_handler = CommandHandler('edit', edit_data)
-    delete_handler = CommandHandler('delete', delete_data)
-
-    # Handler CRUD
-    application.add_handler(login_handler)
-    application.add_handler(read_handler)
-    application.add_handler(create_handler)
-    application.add_handler(edit_handler)
-    application.add_handler(delete_handler)
 
     application.run_polling()
